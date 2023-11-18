2b2d0df038357cbc59da040aa1779b8c50417f94
Nathan <nathanbmiranda@hotmail.com>
2023-11-18 13:48:45 -0300

    nova tela para listagem de jogos criados

--- src/views/ManageQuiz.vue
+++ src/views/ManageQuiz.vue
@@ -1,22 +1,67 @@
-<template>
-    <div>
-        Olá
+<template lang="html">
+  <div>
+    <h1 style="font-size: 3em;" class="p-5 fontt select-none">Listas dos jogos Criados</h1>
+
+    <div class="relative overflow-x-auto p-5" id="game-table">
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" class="px-6 py-3">
+              Nome
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Ações
+            </th>
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <td class="px-6 py-4">
+              Tuba
+            </td>
+            <td class="px-6 py-4 select-none">
+              <i class="bi bi-pencil-square mr-2 cursor-pointer" style="color: #1f74ff; font-size:1.6em;"></i>
+
+              <i class="bi bi-trash cursor-pointer" style="color: #c00808; font-size:1.6em;" @click="deleteGame()"></i>
+            </td>
+
+          </tr>
+        </tbody>
+      </table>
     </div>
-  
+
+  </div>
 </template>
-  
+
 <script>
-  
-  export default {
-    name: 'ManageQuiz',
-  
-    methods: {
-       
+export default {
+  data:()=>({
+
+  }),
+  methods: {
+    deleteGame() {
+      this.$vs.dialog({
+        type:'confirm',
+        color: 'danger',
+        title: `Atenção!`,
+        text: "Você tem certeza que deseja deletar o quiz",
+        // accept: () => this.$refs.fileInput.click(),
+        acceptText: 'Deletar',
+        cancelText: 'Cancelar',
+      })
     }
   }
+}
 </script>
-  
-<style lang="scss">
 
+<style lang="scss">
+ #game-table {
+  table thead tr {
+    background-color: #ffdeb2;
+  }
+  tbody tr {
+    background-color: #fae0be;
+  }
+ }
 </style>
-  
