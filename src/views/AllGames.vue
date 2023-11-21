1103541ae613b0855f7d9ecdf6439d4285ec0e47
NbMiranda <nathanbmiranda@hotmail.com>
2023-11-21 11:14:32 -0300

    criando tela para listagem de games

--- src/views/AllGames.vue
+++ src/views/AllGames.vue
@@ -8,7 +8,7 @@
       pagination 
       max-items="8" 
       search
-      :data="users"
+      :data="listGames"
       noDataText="Sem dados disponiveis"
       id="table-all-games"
       class="p-3"
@@ -26,10 +26,12 @@
     
     <template v-slot:default="{ data }">
       <vs-tr :key="indextr" v-for="(tr, indextr) in data">
-        <vs-td :data="tr.email">{{ tr.email }}</vs-td>
-        <vs-td :data="tr.username">{{ tr.username }}</vs-td>
-        <vs-td :data="tr.id">{{ tr.id }}</vs-td>
-        <vs-td :data="tr.id">play</vs-td>
+        <vs-td :data="tr.name">{{ tr.name }}</vs-td>
+        <vs-td :data="1">{{ 1 }}</vs-td>
+        <vs-td :data="tr.users.name">{{ tr.users.name }}</vs-td>
+        <vs-td :data="tr.id">
+          <i class="bi bi-play cursor-pointer" style="font-size:2em;"></i>
+        </vs-td>
       </vs-tr>
     </template>
       
@@ -39,83 +41,22 @@
   
 <script>
  
+ import gameApi from '@/requests/game';
+ 
   export default {
     name: 'AllGames',
     data:()=>({
-        selected: [],
-        users:[
-          {
-            "id": 1,
-            "name": "Leanne Graham",
-            "username": "Bret",
-            "email": "Sincere@april.biz",
-            "website": "hildegard.org",
-          },
-          {
-            "id": 2,
-            "name": "Ervin Howell",
-            "username": "Antonette",
-            "email": "Shanna@melissa.tv",
-            "website": "anastasia.net",
-          },
-          {
-            "id": 3,
-            "name": "Clementine Bauch",
-            "username": "Samantha",
-            "email": "Nathan@yesenia.net",
-            "website": "ramiro.info",
-          },
-          {
-            "id": 4,
-            "name": "Patricia Lebsack",
-            "username": "Karianne",
-            "email": "Julianne.OConner@kory.org",
-            "website": "kale.biz",
-          },
-          {
-            "id": 5,
-            "name": "Chelsey Dietrich",
-            "username": "Kamren",
-            "email": "Lucio_Hettinger@annie.ca",
-            "website": "demarco.info",
-          },
-          {
-            "id": 6,
-            "name": "Mrs. Dennis Schulist",
-            "username": "Leopoldo_Corkery",
-            "email": "Karley_Dach@jasper.info",
-            "website": "ola.org",
-          },
-          {
-            "id": 7,
-            "name": "Kurtis Weissnat",
-            "username": "Elwyn.Skiles",
-            "email": "Telly.Hoeger@billy.biz",
-            "website": "elvis.io",
-          },
-          {
-            "id": 8,
-            "name": "Nicholas Runolfsdottir V",
-            "username": "Maxime_Nienow",
-            "email": "Sherwood@rosamond.me",
-            "website": "jacynthe.com",
-          },
-          {
-            "id": 9,
-            "name": "Glenna Reichert",
-            "username": "Delphine",
-            "email": "Chaim_McDermott@dana.io",
-            "website": "conrad.com",
-          },
-          {
-            "id": 10,
-            "name": "Clementina DuBuque",
-            "username": "Moriah.Stanton",
-            "email": "Rey.Padberg@karina.biz",
-            "website": "ambrose.net",
-          }
-        ]
+      listGames: [],
     }),
+    methods: {
+      async loadGames() {
+        const games = await gameApi.getAll()
+        this.listGames = games.games
+      }
+    },
+    async mounted() {
+      await this.loadGames();
+    }
   }
 </script>
   
